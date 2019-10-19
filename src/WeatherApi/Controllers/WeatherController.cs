ce93e37a371428ee16ab9fb16b67f6f2f082ad17
BradLewis <22850972+BradLewis@users.noreply.github.com>
2019-10-18 23:00:20 -0400

    initial commit

---
+++ src/WeatherApi/Controllers/WeatherController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WeatherApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeatherController : ControllerBase
+    {
+        private readonly ILogger _logger;
+
+        public WeatherController(ILogger<WeatherController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<int> Get()
+        {
+            return new int[]
+            {
+                1,2,3,4
+            };
+        }
+    }
+}
