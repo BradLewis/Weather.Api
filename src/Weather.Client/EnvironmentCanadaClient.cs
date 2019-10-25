857d3fdc5268a3cb99adc84121e15e413f83bca2
BradLewis <22850972+BradLewis@users.noreply.github.com>
2019-10-24 22:47:14 -0400

    Implement retrieving data between the date range

--- src/Weather.Client/EnvironmentCanadaClient.cs
+++ src/Weather.Client/EnvironmentCanadaClient.cs
@@ -38,15 +38,26 @@ namespace Weather.Client
 
         private async Task<IEnumerable<WeatherModel>> DownloadData(int stationId, DateTime startDate, DateTime endDate)
         {
-            var startYear = startDate.Year;
-            var endYear = endDate.Year;
-            var client = _clientFactory.CreateClient();
+            startDate = new DateTime(startDate.Year, startDate.Month, 1);
+            endDate = new DateTime(endDate.Year, endDate.Month, 1);
+            List<WeatherModel> weatherModels = new List<WeatherModel>();
 
-            var endpoint = $"{_endpoint}?format=csv&stationID={stationId}&Year={startYear}&Month=1&Day=14&timeframe=1&submit=Download+Data";
-            var request = new HttpRequestMessage(HttpMethod.Get, endpoint);
-            var response = await client.SendAsync(request);
-            var result = await ReadCsv(response);
-            return result;
+            using (var client = _clientFactory.CreateClient())
+            {
+                var date = startDate;
+                while (date < endDate)
+                {
+
+                    var endpoint = $"{_endpoint}?format=csv&stationID={stationId}&Year={date.Year}&Month={date.Month}&Day=14&timeframe=1&submit=Download+Data";
+                    var request = new HttpRequestMessage(HttpMethod.Get, endpoint);
+                    var response = await client.SendAsync(request);
+                    var result = await ReadCsv(response);
+                    weatherModels.AddRange(result);
+
+                    date = date.AddMonths(1);
+                }
+            }
+            return weatherModels;
         }
 
         public async Task<IEnumerable<WeatherModel>> GetData(int stationId, DateTime startDate, DateTime endDate)
