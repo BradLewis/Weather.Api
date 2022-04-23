b0104b992134e1bc4d6a96000e75b9d35d199022
Brad Lewis <22850972+BradLewis@users.noreply.github.com>
2022-04-23 13:29:56 -0400

    Reworking CSV reader to support new version

--- src/Weather.Api/Readers/CsvReader.cs
+++ src/Weather.Api/Readers/CsvReader.cs
@@ -1,25 +1,24 @@
 ﻿using CsvHelper.Configuration;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Weather.Api.Models;
 
-namespace Weather.Api.Readers
+namespace Weather.Api.Readers;
+
+public class CsvReader : ICsvReader
 {
-    public class CsvReader : ICsvReader
+    public async Task<IEnumerable<WeatherModel>> ReadCsv<T>(HttpResponseMessage response) where T : ClassMap
     {
-        public async Task<IEnumerable<WeatherModel>> ReadCsv<T>(HttpResponseMessage response) where T : ClassMap
-        {
-            using (var stream = await response.Content.ReadAsStreamAsync())
-            using (var reader = new StreamReader(stream))
-            using (var csvReader = new CsvHelper.CsvReader(reader))
-            {
-                csvReader.Configuration.RegisterClassMap<T>();
-                csvReader.Configuration.MissingFieldFound = null;
-                return csvReader.GetRecords<WeatherModel>().ToList();
-            }
-        }
+        var csvReaderConfiguration = new CsvConfiguration(cultureInfo: CultureInfo.InvariantCulture);
+        csvReaderConfiguration.MissingFieldFound = null;
+        using var stream = await response.Content.ReadAsStreamAsync();
+        using var reader = new StreamReader(stream);
+        using var csvReader = new CsvHelper.CsvReader(reader, csvReaderConfiguration);
+        csvReader.Context.RegisterClassMap<T>();
+        return csvReader.GetRecords<WeatherModel>().ToList();
     }
 }
